24d93b7bc88f2b0f7a1ca0569a57feaeba7e32c9
Logical_IT <>
2023-01-26 22:00:09 +0900

    "main 수정"

--- bank_project/src/page/main.jsx
+++ bank_project/src/page/main.jsx
@@ -67,7 +67,7 @@ export default function Main() {
   
   return (
     <>
-    {isLoggined ? "로그인됨" : "로그인안됨"}
+    {/* {isLoggined ? "로그인됨" : "로그인안됨"} */}
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
