920502022192225faf58aefd367b426d0653eb5d
Logical_IT <>
2023-02-10 14:26:56 +0900

    로그인페이지 하단영역(회원가입 페이지 이동)

--- bank_project/src/component/LoginPage/SignMenuButton.jsx
+++ bank_project/src/component/LoginPage/SignMenuButton.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { LockClosedIcon,} from "@heroicons/react/20/solid";
 import { useNavigate } from 'react-router-dom';
 
-
+     // 로그인 페이지 하단 영역
 export default function SignMenuButton() {
     const navigate = useNavigate();
 
-
+     // 회원가입 페이지 이동
     const openUserAuthPage = () => {
         navigate("/SignIn");
       };
